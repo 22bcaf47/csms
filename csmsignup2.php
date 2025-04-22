af2f1a08cd008d3576f6b94a475946c45dda7c32
SayanthRajesh <156885813+22bcaf47@users.noreply.github.com>
2025-04-22 17:41:48 +0530

    Update csmsignup2.php

--- csmsignup2.php
+++ csmsignup2.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
+$servername = "my-mysql";
 $username = "root";
 $password = "root";
 $dbname = "cms";
-$port = "3307";
 
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 
 if ($conn->connect_error) {
